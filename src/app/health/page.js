2612f13b7d311919388aad34105296370e9e9fc2
kyohoonsim <kyohoonsim@kyohoonsimui-MacBookPro.local>
2023-12-17 00:00:51 +0900

    ENH: 혈압 차트 디자인 수정

--- src/app/health/page.js
+++ src/app/health/page.js
@@ -3,13 +3,14 @@
 import React, { useEffect, useState } from 'react';
 import { useRouter } from "next/navigation"
 import { BloodForm } from "./BloodForm";
-import { LineChart, Line, BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
+import { LineChart, Line, ReferenceArea, XAxis, YAxis, CartesianGrid, Tooltip, Legend, ResponsiveContainer } from 'recharts';
 
 
 export default function Health() {
     const router = useRouter();
     const [myArray, setMyArray] = useState([]);
     const [loading, setLoading] = useState(false);
+    const [chartYn, setChartYn] = useState(false);
 
     const fetchData = async() => {
         if (loading) {
@@ -27,10 +28,16 @@ export default function Health() {
             .then(res=>res.json())
             .then(result=>{
                 if (result.status_code == 200){
+                    console.log(result.data.blood_pressure_list);
                     const newArray = result.data.blood_pressure_list;
-
+                    console.log(newArray.length);
+                    var i;
+                    for (i=0; i<newArray.length; i++){
+                        newArray[i].created_at = new Date(newArray[i].created_at).getTime()
+                    }
                     // setMyArray 함수를 사용하여 상태 업데이트
                     setMyArray(newArray);
+                    setChartYn(true);
                     router.refresh();
                 }
             })
@@ -46,7 +53,7 @@ export default function Health() {
     return (
         <>
         <BloodForm />
-        <div style={{ display: myArray ? "block" : "none"}}>
+        <div style={{ display: chartYn ? "block" : "none"}}>
             <ResponsiveContainer width="100%" height={400}>
                 <LineChart
                 width={500}
@@ -58,14 +65,73 @@ export default function Health() {
                     left: 0,
                     bottom: 10,
                 }}
+                
                 >
-                <CartesianGrid strokeDasharray="3 3" />
-                <XAxis dataKey="created_at" />
-                <YAxis />
-                <Tooltip />
+                <CartesianGrid strokeDasharray="5 5" />
+                <XAxis 
+                    dataKey="created_at" 
+                    type="number" 
+                    scale="time" 
+                    domain={['auto', 'auto']} 
+                    tickFormatter={(value) => new Date(value).toISOString()}
+                />
+                <YAxis domain={[40, 200]}/>
+                <Tooltip 
+                    labelFormatter={(value) => new Date(value).toISOString()}
+                />
                 <Legend />
-                <Line type="monotone" dataKey="high" stroke="#ec4899" activeDot={{ r: 8 }} />
-                <Line type="monotone" dataKey="low" stroke="#2563eb" activeDot={{ r: 4 }} />
+                <Line type="monotone" dataKey="high" stroke="#ec4899" strokeWidth={3} activeDot={{ r: 8 }} />
+                <Line type="monotone" dataKey="low" stroke="#2563eb" strokeWidth={3} activeDot={{ r: 4 }} />
+                <ReferenceArea 
+                    y1={160}
+                    y2={200}
+                    fillOpacity={0}
+                    label={{ position: 'insideBottomLeft', value: '중등도이상 고혈압(수축기)', fill: 'black', fillOpacity: '0.4' }} 
+                />
+                <ReferenceArea 
+                    y1={140}
+                    y2={159}
+                    fillOpacity={0}
+                    label={{ position: 'insideBottomLeft', value: '경도 고혈압(수축기)', fill: 'black', fillOpacity: '0.4' }} 
+                />
+                <ReferenceArea 
+                    y1={120}
+                    y2={139}
+                    fillOpacity={0}
+                    label={{ position: 'insideBottomLeft', value: '고혈압 전단계(수축기)', fill: 'black', fillOpacity: '0.4' }} 
+                />
+                <ReferenceArea 
+                    y1={100}
+                    y2={120}
+                    fill="green"
+                    fillOpacity={0.3}
+                    label={{ position: 'insideBottomLeft', value: '정상(수축기)', fill: 'black', fillOpacity: '0.4' }} 
+                />
+                <ReferenceArea 
+                    y1={100}
+                    y2={120}
+                    fillOpacity={0}
+                    label={{ position: 'insideBottomRight', value: '중등도이상 고혈압(이완기)', fill: 'black', fillOpacity: '0.4' }} 
+                />
+                <ReferenceArea 
+                    y1={90}
+                    y2={99}
+                    fillOpacity={0}
+                    label={{ position: 'insideBottomRight', value: '경도 고혈압(이완기)', fill: 'black', fillOpacity: '0.4' }} 
+                />
+                <ReferenceArea 
+                    y1={80}
+                    y2={89}
+                    fillOpacity={0}
+                    label={{ position: 'insideBottomRight', value: '고혈압 전단계(이완기)', fill: 'black', fillOpacity: '0.4' }} 
+                />
+                <ReferenceArea 
+                    y1={60}
+                    y2={80}
+                    fill="green"
+                    fillOpacity={0.3}
+                    label={{ position: 'insideBottomRight', value: '정상(이완기)', fill: 'black', fillOpacity: '0.4' }} 
+                />
                 </LineChart>
             </ResponsiveContainer>
         </div>
@@ -73,8 +139,9 @@ export default function Health() {
 
         <ul className="list-inside list-disc my-8">
             {myArray.map((blood) => (
-                <li key={blood.created_at}>{blood.created_at} ({blood.location}): {blood.high}/{blood.low}</li>
-
+                <li key={blood.created_at}>
+                    {new Date(blood.created_at).toISOString()} ({blood.location}): {blood.high}/{blood.low}
+                </li>
             ))}
         </ul>
         </>
